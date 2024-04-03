087ddc534302312e6d94c43722e67d9952b6241a
Tung Duong <biuss@Tungs-MacBook-Air.local>
2024-04-03 11:28:59 +0700

    Initialized

---
+++ src/main/java/com/tungdd/k8sdemo/K8sdemoApplication.java
@@ -0,0 +1,13 @@
+package com.tungdd.k8sdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(K8sdemoApplication.class, args);
+    }
+
+}
