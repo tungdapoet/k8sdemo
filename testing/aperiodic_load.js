2b7bccc3e3ccdd35e999e8c6f303c852329e2c47
tungdapoet <tungdd.hrt@gmail.com>
2024-05-16 03:39:44 +0700

    Add test scenarios & Service

--- testing/aperiodic_load.js
+++ testing/aperiodic_load.js
@@ -1,15 +1,18 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
-export const options = {
+export let options = {
     stages: [
         { duration: '1m', target: 100 },
-        { duration: '5m', target: 100 },
+        { duration: '2m', target: 100 },
+        { duration: '30s', target: 300 },
+        { duration: '1m30s', target: 50 },
+        { duration: '1m', target: 200 },
+        { duration: '2m', target: 200 },
         { duration: '1m', target: 0 },
     ],
     thresholds: {
         'http_req_duration': ['p(95)<500'],
-        'http_req_failed': ['rate<0.01'],
     },
 };
 
