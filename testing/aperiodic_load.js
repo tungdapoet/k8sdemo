db8497db620931deffbe121f657f060da1661103
tungdapoet <tungdd.hrt@gmail.com>
2024-05-17 15:59:26 +0700

    update tests

--- testing/aperiodic_load.js
+++ testing/aperiodic_load.js
@@ -5,10 +5,10 @@ export let options = {
     stages: [
         { duration: '1m', target: 100 },
         { duration: '2m', target: 100 },
-        { duration: '30s', target: 300 },
+        { duration: '30s', target: 180 },
         { duration: '1m30s', target: 50 },
-        { duration: '1m', target: 200 },
-        { duration: '2m', target: 200 },
+        { duration: '1m', target: 180 },
+        { duration: '2m', target: 140 },
         { duration: '1m', target: 0 },
     ],
     thresholds: {
--- testing/periodic_load.js
+++ testing/periodic_load.js
@@ -3,12 +3,12 @@ import { sleep } from 'k6';
 
 export let options = {
     stages: [
-        { duration: '1m', target: 200 },
-        { duration: '1m', target: 200 },
+        { duration: '1m', target: 100 },
+        { duration: '1m', target: 100 },
         { duration: '1m', target: 0 },
         { duration: '1m', target: 0 },
-        { duration: '1m', target: 200 },
-        { duration: '1m', target: 200 },
+        { duration: '1m', target: 100 },
+        { duration: '1m', target: 100 },
         { duration: '1m', target: 0 },
         { duration: '1m', target: 0 },
     ],
