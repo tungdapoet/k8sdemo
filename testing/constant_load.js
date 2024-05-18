dd2f23fa081f84382efb5a10bdb164ba6b154287
tungdapoet <tungdd.hrt@gmail.com>
2024-05-18 13:14:48 +0700

    Keo giai thoi gian test

--- testing/constant_load.js
+++ testing/constant_load.js
@@ -3,9 +3,9 @@ import { sleep } from 'k6';
 
 export const options = {
     stages: [
-        { duration: '1m', target: 40 },
-        { duration: '1m', target: 40 },
-        { duration: '1m', target: 0 },
+        { duration: '2m', target: 40 },
+        { duration: '2m', target: 40 },
+        { duration: '2m', target: 0 },
     ],
     thresholds: {
         'http_req_duration': ['p(95)<1000'],
