492d3121f0ab418aa7b637274df93c70c4926249
tungdapoet <tungdd.hrt@gmail.com>
2024-05-18 14:21:36 +0700

    Keo giai thoi gian test

--- testing/aperiodic_load.js
+++ testing/aperiodic_load.js
@@ -1,31 +1,31 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
-export let options = {
+export const options = {
     stages: [
-        { duration: '1m', target: 0 },
-        { duration: '1m', target: 30 },
-        { duration: '2m', target: 10 },
-        { duration: '1m', target: 50 },
-        { duration: '1m', target: 20 },
-        { duration: '2m', target: 40 },
-        { duration: '1m', target: 0 },
-        { duration: '1m', target: 30 },
-        { duration: '2m', target: 10 },
-        { duration: '2m', target: 50 },
-        { duration: '1m', target: 0 },
+        { duration: '2m', target: 20 },
+        { duration: '3m', target: 20 },
+        { duration: '1m', target: 40 },
+        { duration: '3m', target: 40 },
+        { duration: '2m', target: 0 },
+        { duration: '2m', target: 0 },
+        { duration: '1m', target: 60 },
+        { duration: '4m', target: 60 },
+        { duration: '2m', target: 20 },
+        { duration: '3m', target: 20 },
+        { duration: '2m', target: 0 },
     ],
     thresholds: {
         'http_req_duration': ['p(95)<1000'],
+        'http_req_failed': ['rate<0.01'],
     },
 };
 
 export default function () {
-    const res = http.get('http://localhost:30080/info/hostname');
+    const res = http.get('http://localhost:8080/info/hostname');  // Thay thế bằng endpoint của bạn
 
     if (res.status !== 200) {
         console.error(`Request failed. Status: ${res.status}`);
     }
-
-    sleep(1);
+    sleep(1);  // Mỗi VU gửi yêu cầu mỗi giây
 }
